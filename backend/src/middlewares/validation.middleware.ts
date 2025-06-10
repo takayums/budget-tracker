3beaf006ed0150ea64dbce4c05a1a699f90c12d5
takayums <takayums20@gmail.com>
2025-06-10 08:48:23 +0700

    be: add middleware error validator express

---
+++ backend/src/middlewares/validation.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+function validateRequest(req: Request, res: Response, next: NextFunction) {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    res.status(400).json({
+      success: false,
+      message: "Validaton Input Gagal",
+      errors: result.array().map((e) => ({ params: e.type, msg: e.msg })),
+    });
+  }
+  next();
+}
